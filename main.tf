e8f025d5f7b986b6227caa324f62e68c646587eb
Ted <tedv@live.com>
2023-10-10 09:02:24 +0200

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-data "azurerm_resource_group" "app" {
-  name     = var.resource_group_name
-}
-
 resource "azurerm_service_plan" "app" {
   name                = var.service_plan_name
-  resource_group_name = data.azurerm_resource_group.app.name
-  location            = data.azurerm_resource_group.app.location
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = var.linux_web_app_name
-  resource_group_name = data.azurerm_resource_group.app.name
-  location            = azurerm_service_plan.app.location
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
   service_plan_id     = azurerm_service_plan.app.id
   site_config {}
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "resource_group_location" {
+  type = string
+}
+
 variable "service_plan_name" {
   type = string
 }
