1f7aa230af3b683f8044108932e5bbb16bd49698
tvdv <tedv@live.com>
2023-10-10 08:16:48 +0200

    update

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "app" {
+  name     = var.resource_group_name
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = var.service_plan_name
+  resource_group_name = azurerm_resource_group.app.name
+  location            = azurerm_resource_group.app.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = var.linux_web_app_name
+  resource_group_name = azurerm_resource_group.plan.name
+  location            = azurerm_service_plan.app.location
+  service_plan_id     = azurerm_service_plan.app.id
+  site_config {}
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "service_plan_name" {
+  type = string
+}
+
+variable "linux_web_app_name" {
+  type = string
+}
