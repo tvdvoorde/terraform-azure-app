dd5fa14e65f8851d963005e1add518c122035b1a
Ted <tedv@live.com>
2023-10-10 09:14:55 +0200

    update

--- variables.tf
+++ variables.tf
@@ -8,6 +8,15 @@ variable "resource_group_location" {
 
 variable "service_plan_name" {
   type = string
+  description = "Specifies the app service plan name - must start with \"plan-\"."
+
+  validation {
+    condition     = substr(var.image_id, 0, 5) == "plan-"
+    error_message = "The plan name must start with \"plan-\"."
+  }  
+}
+
+
 }
 
 variable "linux_web_app_name" {
