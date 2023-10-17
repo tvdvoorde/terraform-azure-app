828dc804f116bf6d2cb28734d257b1dd045f8eb5
tvdv <tedv@live.com>
2023-10-17 16:55:01 +0200

    Update test.sh

--- consumer-example/test.sh
+++ consumer-example/test.sh
@@ -1,10 +1,1 @@
-inspec exec --chef-license=accept-silent --input=HOST=x$* --reporter html:test.html -- ../tests/inspec/profiles/access 
-if [ $? -eq 0 ] 
-then
-  echo '{"passed": "true"}'|jq .
-  exit 0
-else
-  echo '{"passed": "false"}'|jq .
-  exit 1
-fi
 
