2ac4b0836f3ed3e6f536191403fc3bf9fa53b39c
tvdv <tedv@live.com>
2023-10-17 16:00:32 +0200

    Update test.sh

--- tests/inspec/test.sh
+++ tests/inspec/test.sh
@@ -1,5 +1,6 @@
 sleep 60
 inspec exec --chef-license=accept-silent --input=HOST=$1 --reporter html:test.html -- $2/profiles/access 
+# this needs work for better checking if all checks were valid
 if [ $? -eq 0 ] 
 then
   echo '{"passed": "true"}'|jq .
